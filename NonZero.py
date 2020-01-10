41b3859250bc665a4d62fa1caef7dc721741641e
Chigrina Anna <achigrina@korusconsulting.ru>
2020-01-10 14:03:27 +0300

    13

--- NonZero.py
+++ NonZero.py
@@ -1,2 +1,10 @@
 #13 задание: Дан массив из 10 чисел. Подсчитать количество не нулевых элементов массива.
 import numpy as np
+
+n = 10
+
+list = [int(input('Введите элемент массива ')) for i in range(n)]
+
+count_nz=np.count_nonzero(list)
+print ("Количество нулевых элементов: ", n-count_nz)
+print("Количество ненулевых элементов: ", count_nz)
