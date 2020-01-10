704b19f112d67841b150c4bfa50347d9b66b8f26
Chigrina Anna <achigrina@korusconsulting.ru>
2020-01-10 14:05:49 +0300

    15

--- NonZero.py
+++ NonZero.py
@@ -6,6 +6,7 @@ n = 10
 list = [int(input('Введите элемент массива ')) for i in range(n)]
 
 count_nz=np.count_nonzero(list)
+
 print ("Количество нулевых элементов: ", n-count_nz)
 print("Количество ненулевых элементов: ", count_nz)
 
